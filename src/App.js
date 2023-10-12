1302bf2df23a55b683232d2a8a9d6b8e25821c65
lcminh <lcminhthpt2000@gmail.com>
2023-10-12 23:59:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import Header from './components/layout/Header';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Header from "./components/layout/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const categories = ["NODE", "REACT", "JS", "CSS", "HTML"];
 
   return (
     <div>
-      <Header categories={categories}/>
-      <Outlet/>
+      <Header categories={categories} />
+      <Outlet />
     </div>
   );
 }
--- src/components/CssBook.js
+++ src/components/CssBook.js
@@ -1,20 +1,23 @@
 import Books from "./Books";
-import React, {useEffect, useState} from "react";
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function CssBook(props) {
-    const [books, getBooks] =useState([]);
+function CssBook() {
+  const [books, getBooks] = useState([]);
 
-    useEffect(() => {
-        axios.get("http://localhost:3001/books").then(response =>{
-        getBooks(response.data)
-        })
-    }, [])
-    return(
-        <Books title={"CSS"} books={books.filter(el => {
-            return el.type === "CSS";
-        })}/>
-    )
+  useEffect(() => {
+    axios.get("http://localhost:3001/books").then((response) => {
+      getBooks(response.data);
+    });
+  }, []);
+  return (
+    <Books
+      title={"CSS"}
+      books={books.filter((el) => {
+        return el.type === "CSS";
+      })}
+    />
+  );
 }
 
 export default CssBook;
